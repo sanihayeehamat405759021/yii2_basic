b0fff21ffe495552e4cf806651c4a250cc8ef702
sanihayeehamat405759021 <sani.h@yru.ac.th>
2017-05-23 15:06:29 +0700

    firdows รับค่าผ่าน $_GET

--- controllers/HelloController.php
+++ controllers/HelloController.php
@@ -5,16 +5,22 @@ namespace app\controllers;
 class HelloController extends \yii\web\Controller{
 
 	public function actionIndex(){
-return $this->render('index',['firstname'=>'ซานี',
-	'lastname'=>'หะยีฮามะ'
-	'weigth'=>$weigth
+		$weigth = 40;
+	return $this->render('index',[
+		'firstname'=>'ซานี',
+		'lastname'=>'หะยีฮามะ',
+		'weigth'=>$weigth
 
-	]);
-	}
-
+		]
+	);
 }
+
 public function actionProfile(){
 	return $this->render('profile');
 }
-
+public function
+actionProfile($id,$brand=null){
+	
+}
+}
 ?>
